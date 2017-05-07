2272158619ca39e9a3da3307f7a2a7bd1ec9bacb
Alexander Falb <fal3xx@gmail.com>
2017-05-07 19:11:40 +0200

    docs: add javadoc to exception

--- src/main/java/com/github/elexx/guavajmx/JmxRegistrationException.java
+++ src/main/java/com/github/elexx/guavajmx/JmxRegistrationException.java
@@ -1,7 +1,7 @@
 package com.github.elexx.guavajmx;
 
 /**
- * TODO alexander: create javadoc!
+ * If anything went wrong during registration this exception gets thrown. Have a look at the {@link #getCause()} for a more detailed reason.
  */
 public class JmxRegistrationException extends Exception {
     JmxRegistrationException(String message, Throwable cause) {
